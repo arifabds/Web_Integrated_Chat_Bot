a14e96443bf65711830c06687e0111c8bc8a0171
arifabds <92270810+arifabds@users.noreply.github.com>
2025-02-20 20:31:41 +0300

    redeploy için

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from backend.Application.app import create_app
 
 app = create_app()  
 
+
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8080))  
     app.run(host="0.0.0.0", port=port, threaded=True)
